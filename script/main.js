e8056d5df92b9cac21e18081421518c7d70e1bab
mcdiaz <mcarolinadiaz0@gmail.com>
2019-01-18 13:05:39 -0300

    main con ejemplos y tuple
    main con ejemplos y tuple

--- script/main.js
+++ script/main.js
@@ -1,6 +1,25 @@
 
 console.log("Todo bien1");
 
+class Tuple {
+	constructor(blob, time){//time debe ser de tipo date - blob contiene el blob en cuestion
+		this.blob=blob;
+		this.time=time;
+		
+	}
+	blob(){
+		return blob;
+	}
+	time(){
+		return time;
+	}
+	setTime(milis){
+		this.time.setMilliseconds(milis);
+	}
+	
+}
+
+
 class Rectangle {//el primero se inicializara con (0,0,800,400)
 	constructor(x,y,w,h){
 		this.x=x;
@@ -32,11 +51,11 @@ class Rectangle {//el primero se inicializara con (0,0,800,400)
 	setHeight(newH){
 		this.height=newH;
 	}
-	contains(obj){
-		return((obj.x <= this.x + this.width) &&
-			(obj.y <= this.y + this.height) && 
-			(obj.x >= this.x - this.width) && 
-			(obj.y >= this.y - this.height));
+	contains(obj){//contiene al menos la mitad del objeto en cuestion
+		return((obj.x + (obj.width)/2 <= this.x + this.width) &&
+			(obj.y + (obj.height)/2 <= this.y + this.height) && 
+			(obj.x + (obj.width)/2 >= this.x - this.width) && 
+			(obj.y + (obj.height)/2 >= this.y - this.height));
 	}
 }
 
@@ -75,6 +94,7 @@ class Quadtree {
 		if(!this.bounds.contains(obj)){//Si no lo puede contener al objeto por lo limites, retorna false
 			return false;
 		}else{
+			
 			if(this.objects.length<this.maxObjects && !this.splited){//Si la cantidad de objetos no excede la cantidad maxima permitida de objetos, inserta y ademas si aun no esta dividido
 				this.objects.push(obj);
 				return true;
@@ -98,17 +118,56 @@ class Quadtree {
 }
 }
 class Scene{
-	constructor(objMax){
+	constructor(objMax,widthMax,hightMax,timeInit){
 		this.objMax=objMax;
-		this.bounds=new Reactangle(0,0,800,400);
+		this.bounds=new Reactangle(0,0,widthMax,hightMax);
 		this.quadtree= new Quadtree(bounds,objMax/4);
+		this.timeInit=timeInit;
+		this.timeLimit;
+		this.objects=new Array();
 	}
-	
+	objMax(){
+		return objMax;
+	}
+	timeInit(){
+		return timeInit;
+	}
+	timeLimit(){
+		return timeLimit;
+	}
+	insert(obj){
+		if(objects.length < ObjMax){
+			var tuple = new Tuple(obj,timeInit);
+			if(quadtree.insert(obj)){//al insert en el quadtree hay que agregar si hay o no colision
+				objects.push(obj);
+			}
+			return true;
+		}
+		else return false;
+	}
 }
 
 
-$.getJSON('script/dataset_small.json')
-    .done(function(response) {//Se escribe el codigo aca adentro, utilizando response object como el contenedor, ya que getJSON es asincronico y no se puede pasar el objeto afuera del json
+function prueba(){
+	var i,x,y,w,h;
+	w=4;h=3;x=0;y=0;
+	var obj;
+	var bo=new Rectangle(0,0,10,7);
+	var quad=new Quadtree(bo,1);
+	//console.log(quad);
+	for(i=0 ; i<7 ; i++){
+		obj=new Rectangle(x,y,w,h);
+		quad.insert(obj);
+		x=x+2;
+		y++;
+	}
+	console.log(quad);
+	
+}
+prueba();
+
+$.getJSON('dataset_example.json')
+    .done(function(response) {//Se escribe el codigo aca adentro, utilizando response object como el contenedor, ya que getJSON es asincronico y no se puede pasar el objecto afuera del json
 		var data = response.data;
 		
 		function sortBlobs(){
@@ -116,8 +175,8 @@ $.getJSON('script/dataset_small.json')
 		var i;
 		for(i = 0; i < data.length; i++){
 			var j;
-			blobs = data[i].lightweight_blobs;
-			blobs.sort(function(a,b){//ordena los blobs de cada tracked blob ascendentemente segun la funcion de comparacion
+			blob = data[i].lightweight_blobs;
+			blob.sort(function(a,b){//ordena los blobs de cada tracked blob ascendentemente segun la funcion de comparacion
 					return -new Date(b.time) + +new Date(a.time);
 				});   
 		}
@@ -126,28 +185,80 @@ $.getJSON('script/dataset_small.json')
 		});
 
 		}
-		
 		sortBlobs();
+		//console.log(data);
 		
-		function prueba(){
-			var i,x,y,w,h;
-			w=4;h=3;x=0;y=0;
-			var obj;
-			var bo=new Rectangle(0,0,10,7);
-			var quad=new Quadtree(bo,5);
-			//console.log(quad);
-			for(i=0 ; i<7 ; i++){
-				obj=new Rectangle(x,y,w,h);
-				quad.insert(obj);
-				x=x+2;
-				y++;
+		/*
+		
+		
+		//Luego de ordenar, empieza el codigo llamando a las clases y todo hecho fuera
+		var scenesList = new Array();
+		var time= new Date(0,0,0,0,0);//time inicializado en 00:00:00
+		
+		function insertInScene(obj){
+			if(scenesList.length == 0){//Si es la primer Escena
+					var scene= new Scene(20,800,480,time);
+					
 			}
-			console.log(quad);
-			//Sasasasas
+			else{//Si hay mas escenas
+				var i;
+				for(i=0;i<scenesList.length;i++){
+					
+				}
+			}
+			
+			
+				
 		}
-		prueba();
-	
-    }
+		
+		var i;
+		var blobs;
+		for(i = 0; i < data.length; i++){
+			var j;
+			blobs=data[i].lightweight_blobs;
+			for(j=0 ; j < blobs.length ; j++){
+				insertInScene(blobs[j]);
+			}
+		}
+		*/
+		
+
+		
+		//var FinalList = new Array();
+		var time = new Date(0,0,0,0,0);
+		var i;
+		var blob;
+		var blobsList = new Array();
+		for(i = 0;i<data.length;i++){
+			var j;
+			var trackblobs;
+		
+			blob=new Tuple(data[i], time);
+
+				//cambia tiempo de cada tracked blob
+			if(blobsList.length == 0){
+				blobsList.push(blob);
+				//blob=new Tuple(data[i], blobsList[blobsList.length - 1].time);
+
+			}
+			else {
+				blob=new Tuple(data[i], blobsList[blobsList.length - 1].time);
+				//console.log(blobsList[blobsList.length - 1]);
+				blob.setTime(10000 + blob.time.getMilliseconds());
+				blobsList.push(blob);
+				console.log(blobsList[blobsList.length - 1]);
+			}
+
+			}
+			
+			console.log(blobsList[2].time);
+
+		
+		
+		
+		
+	}
+    
 	);
 	
 
